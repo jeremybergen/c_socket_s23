eb4c249134cf17ca55a6dd89e1dbe9fdb5324981
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-04-19 21:21:13 -0600

    mostly working server and client file transfer

--- client.c
+++ client.c
@@ -8,23 +8,88 @@ c socket client
 #include <string.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
     int clientSocket;
     struct sockaddr_in address;
-    char buffer[1024] = {0};
+    // char buffer[1024] = {0};
+    char *buffer;
+    size_t bufferLen = 0;
+    int connectStatus;
     
     address.sin_family = AF_INET;
-    address.sin_port = htons(1234);
 
+    address.sin_port = htons(1234);
+    inet_pton(AF_INET, "127.0.0.1", &(address.sin_addr));
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    connect(clientSocket, (struct sockaddr *)&address, sizeof(address));
+    connectStatus = connect(clientSocket, (struct sockaddr *)&address, sizeof(address));
+    printf("Connection status is: %d\n", connectStatus);
+    if(connectStatus == -1) {
+        fprintf(stderr, "Error on connect --> %s\n", strerror(errno));
+    }
 
-    strcat(buffer, "Hello\n");
-    send(clientSocket, buffer, 6, 0);
-    recv(clientSocket, buffer, 1024, 0);
-    printf("Received from server: %s\n", buffer);
+    // strcat(buffer, "Hello\n");
+    
+    // buffer = readline("What do you want to send to the server? ");
+
+
+    // while(1) {
+        // printf("DEBUG: creating sendBuffer that we aren't using\n");
+        // char sendBuffer[1024] = {0};
+        // printf("DEBUG: memset buffer");
+        memset(buffer, 0, sizeof(buffer));
+
+        // printf("What mode do you want to go in? ");
+        // getline(&buffer, &bufferLen, stdin);
+        // // send(clientSocket, buffer, bufferLen, 0);
+
+        // strcat(sendBuffer, buffer);
+        // memset(buffer, 0, bufferLen);
+
+        // printf("What do you want to send to the server? ");
+        // getline(&buffer, &bufferLen, stdin);
+        // strcat(sendBuffer, buffer);
+
+        // send(clientSocket, sendBuffer, strlen(sendBuffer), 0);
+        // printf("DEBUG: before first recv\n");
+        recv(clientSocket, buffer, 1024, 0);
+        printf("Received from server: %s\n", buffer);
+
+        ssize_t fileSize = atol(buffer);
+        ssize_t remainingBytes = fileSize;
+        memset(buffer, 0, strlen(buffer));
+
+        sprintf(buffer, "Received file size");
+        send(clientSocket, buffer, strlen(buffer), 0);
+        memset(buffer, 0, strlen(buffer));
+
+        int fileDescriptor = open("outputFile", O_CREAT | O_WRONLY, S_IRWXU | S_IRWXG | S_IRWXO);
+        // printf("fileDescriptor: %d\n", fileDescriptor);
+        while(remainingBytes > 0) {
+            ssize_t recvBytes = recv(clientSocket, buffer, 1024, 0);
+            // printf("DEBUG: strlen(buffer): %ld\n", strlen(buffer));
+            ssize_t writeBytes = write(fileDescriptor, buffer, recvBytes);
+            if (writeBytes != recvBytes) {
+                printf("writeBytes: %ld\nrecvBytes: %ld\n", writeBytes, recvBytes);
+            }
+
+            remainingBytes -= recvBytes;
+            printf("DEBUG: recvBytes: %ld\n", recvBytes);
+            memset(buffer, 0, recvBytes);
+        }
+
+        printf("Closing fileDescriptor\n");
+        close(fileDescriptor);
+
+        // memset(buffer, 0, bufferLen);
+    // }
 
     return 0;
 }
--- server.c
+++ server.c
@@ -8,11 +8,15 @@ C Socket Server
 #include <string.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 // int main(int argc, char *argv[]) {
 int main(int argc, char **argv) {
     int serverSocket;
     int clientSocket;
+    int fileDescriptor;
     struct addrinfo hints, *res;
     struct sockaddr_in address;
     int addrsize = sizeof(struct sockaddr_storage);
@@ -20,6 +24,9 @@ int main(int argc, char **argv) {
 
     char buffer[1024] = {0};
     int readBytes = 0;
+
+    char *fileName = "client.out";
+    struct stat fileStat;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
@@ -33,13 +40,60 @@ int main(int argc, char **argv) {
     bind(serverSocket, res->ai_addr, res->ai_addrlen);
 
     listen(serverSocket, 1);
-
-    clientSocket = accept(serverSocket, (struct sockaddr *)&address, (socklen_t *)&addrsize);
+    
     while(1) {
-        readBytes = recv(clientSocket, buffer, 1024, 0);
-        printf("We received: %s\n", buffer);
-        send(clientSocket, buffer, readBytes, 0);
-        memset(buffer, 0, readBytes);
+        clientSocket = accept(serverSocket, (struct sockaddr *)&address, (socklen_t *)&addrsize);
+    
+        while(1) {
+            // char *sendBuf;
+            char sendBuf[1024] = {0};
+            fileDescriptor = open(fileName, O_RDONLY);
+            fstat(fileDescriptor, &fileStat);
+            printf("Filesize: %ld\n", fileStat.st_size);
+
+            sprintf(sendBuf, "%ld", fileStat.st_size);
+            printf("DEBUG: sendBuf: %s\n", sendBuf);
+            send(clientSocket, sendBuf, strlen(sendBuf), 0);
+            printf("DEBUG after send\n");
+
+            recv(clientSocket, sendBuf, 1024, 0);
+            printf("Received: %s\n", sendBuf);
+            memset(sendBuf, 0, strlen(sendBuf));
+
+            off_t remainingData = fileStat.st_size;
+            while(remainingData > 0) {
+                ssize_t readBytes = read(fileDescriptor, sendBuf, 1024);
+                if(readBytes == -1) {
+                    printf("File read error\n");
+                    break;
+                }
+                printf("readBytes: %ld\n", readBytes);
+                send(clientSocket, sendBuf, readBytes, 0);
+                remainingData -= readBytes;
+                
+                // break;
+            }
+
+            
+            close(fileDescriptor);
+            close(clientSocket);
+            break;
+            // recv(clientSocket, buffer, 2, 0);
+            // if(strcmp(buffer, "2\n") == 0) {
+            //     printf("you are in mode 2\n");
+            // } else {
+            //     printf("you are in mode 1\n");
+            // }
+            // readBytes = recv(clientSocket, buffer, 1024, 0);
+            // printf("We received: %s\n", buffer);
+            // char newBuffer[1024] = {0};
+            // strcat(newBuffer, "From server: ");
+            // strcat(newBuffer, buffer);
+            // send(clientSocket, newBuffer, strlen(newBuffer), 0);
+
+            // memset(buffer, 0, readBytes);
+            // memset(newBuffer, 0, strlen(newBuffer));
+        }
         
     }
 
